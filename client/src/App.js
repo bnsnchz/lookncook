9c6895fd455b4afb708b9265ee5feceaff180310
Albert Kim <miktrebla92@gmail.com>
2018-07-25 14:44:52 -0700

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import SearchRecipes from './components/SearchRecipes';
+import SavedRecipes from './components/SavedRecipes';
+import SubmitRecipes from './components/SubmitRecipes';
 
-class App extends Component {
-  render() {
+const App = () => {
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+      <Router>
+        <Wrapper>
+          <Header />
+            <Route exact path = '/search' component={SearchRecipes} />
+            <Route exact path = '/saved' component={SavedRecipes} />
+            <Route exact path = '/submit' component={SubmitRecipes} />
+          <Footer />
+        </Wrapper>
+      </Router>
+    )
 }
 
 export default App;
