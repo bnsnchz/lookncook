0aa3ca0568d36d35b056f5c44661264fe373ef95
Phazeon <tjl82996@gamil.com>
2018-07-26 11:20:08 -0700

    Updated server with mongoose and package.json

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Define middleware here
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
+// Define app.use(routes);
+app.use(routes);
+
+// Set up promises with mongoose
+mongoose.Promise = global.Promise;
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/recipe_db",
+  {
+    useMongoClient: true
+  }
+);
 
 // Send every other request to the React app
 // Define any API routes before this runs
