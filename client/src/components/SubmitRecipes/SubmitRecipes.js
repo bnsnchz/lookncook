2aa2c0f9ca15c5a605d310e044c8a83749f5dd88
Albert Kim <miktrebla92@gmail.com>
2018-07-26 13:30:22 -0700

    re-updated submit page

--- client/src/components/SubmitRecipes/SubmitRecipes.js
+++ client/src/components/SubmitRecipes/SubmitRecipes.js
@@ -3,20 +3,24 @@ import React, {Component} from 'react';
 class SubmitRecipes extends Component {
     state = {
         recipeTitle:'',
-        instructionList:[],
-        ingredientList:[]
+        instructionList:'',
+        ingredientList:''
         //wait for models to finish and replicate model format
 
     }
     handleInputChange = event => {
         event.preventDefault();
-        event.target = {}
+        const {name, value} = event.target
+        this.setState({
+            [name]:value
+        })
         this.handleBulletPoint();
     }
 
     handleFormSubmit= event => {
         event.preventDefault();
-
+        console.log('formSubmit clicked');
+        //axios request to POST recipe
     }
 
     handleBulletPoint = (event) => {
@@ -33,40 +37,46 @@ class SubmitRecipes extends Component {
         return (
             <div>
                 <form>
-                    <label for='recipeTitle'>Recipe Title</label>
+                    <label htmlFor='recipeTitle'>Recipe Title</label>
                     <br/>
                     <input 
                         type = 'text' 
                         id='recipeTitle' 
                         name='recipeTitle' 
                         value={this.state.recipeTitle}
+                        onChange={this.handleInputChange}
                         className = 'formTitle'
                         placeholder='Required'>
                     </input>
                     <br/>
-                    <label for='ingredientList'>Ingredient List</label>
+                    <label htmlFor='ingredientList'>Ingredient List</label>
                     <br/>
                     <textarea 
                         type = 'text' 
                         id='ingredientList' 
                         name='ingredientList'
                         value= {this.state.ingredientList} 
+                        onChange={this.handleInputChange}
                         placeholder='Ingredients...'>
                     </textarea>
                     <br/>
-                    <label for='recipeTitle'>Instruction</label>
+                    <label htmlFor='recipeTitle'>Instruction</label>
                     <br/>
                     <textarea 
                         type = 'text' 
                         id='instructionList' 
                         name='instructionList' 
                         value={this.state.instructionList}
+                        onChange={this.handleInputChange}
                         placeholder='Instructions' 
                         rows = '10' 
                         cols = '100'>
                     </textarea>
                     <br/>
-                    <button id="submitRecipe">Submit Recipe</button>
+                    <button id="submitRecipe" 
+                        onClick={this.handleFormSubmit}>
+                        Submit Recipe
+                    </button>
                 </form>
                
             </div>
