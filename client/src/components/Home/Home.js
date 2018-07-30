c2c6d6cb3b4291882ed769e6a5521ae55129ba48
bnsnchz <bsanchez4488@gmail.com>
2018-07-30 15:34:54 -0700

    updating -ben

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -9,8 +9,8 @@ class Home extends Component {
                     <div className="homeDiv">
                         <h3>About</h3>
                             <p>Potato clavela blanca kerr's pink atlantic adirondack red bildtstar marcy. Vivaldi linda carola marquis almond bonnotte bildtstar, ratte alturas megachip german butterball keuka gold golden wonder red norland. Kennebec annabelle almond camota ranger russet, marcy ratte arran victory norgold russet stobrawa alturas. Atlantic bonnotte cielo marquis french fries, alturas atlantic vivaldi red norland kipfler. Golden wonder maris piper annabelle home guard linda sieglinde arran victory. Estima sirco pink eye kipfler monalisa selma. Ratte yellow finn, tater pike belle de fontenay innovator anya stobrawa. Annabelle irish cobbler blazer russet clavela blanca maris piper. Arran victory megachip pink fir apple atlantic avalanche primura annabelle, norgold russet primura arran victory vivaldi marcy.
-                            <br/>
-                            <br/>
+                            </p>
+                            <p>
                             Cielo clavela blanca ranger russet keuka gold spud flava german butterball, kennebec superior rooster stobrawa apline bintje. Red pontiac norgold russet adirondack red annabelle maris piper golden wonder fingerling. King edward shepody clavela blanca blazer russet nicola marfona avalanche, kipfler rooster blazer russet pink eye kerr's pink german butterball. Flava cara, goldrush superior sirco annabelle kestrel keuka gold. Pike marfona fianna lady balfour red pontiac fingerling potato. Annabelle kestrel amandine atlantic, norgold russet pink fir apple rooster irish cobbler yellow finn jersey royal bannock russet. King edward nicola canela russet estima clavela blanca cara red pontiac. Flava superior agata alturas sieglinde spud.
                             </p>
                     </div>
